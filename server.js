bed5af33bdbfe858f21cf53f269aba4766fcaa6a
Max <maxjoyce99@gmail.com>
2024-12-20 11:40:08 -0500

    Added another line to accept requests from other domains

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use((req,res,next) => {
 //Allows CORS for testing
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
